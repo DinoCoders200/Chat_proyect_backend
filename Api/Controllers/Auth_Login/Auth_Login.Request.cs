e752245b17f2701201b7ebe44fd6badb84a4654e
IgnacioV <ignaus.v@gmail.com>
2026-07-08 23:01:00 -0400

    refactor: remove old Auth_Login folder

--- Api/Controllers/Auth_Login/Auth_Login.Request.cs
+++
@@ -1,8 +0,0 @@
-namespace custom_chat_backend.Api.Controllers.Auth_Login;
-
-public sealed record Auth_LoginRequest(
-    string User,
-    string Password)
-{
-    public const string Route = "/Auth_Login";
-}
--- Api/Controllers/Auth_Login/Auth_Login.Response.cs
+++
@@ -1,5 +0,0 @@
-namespace custom_chat_backend.Api.Controllers.Auth_Login;
-
-public sealed record Auth_LoginResponse(
-    bool Success
-);
